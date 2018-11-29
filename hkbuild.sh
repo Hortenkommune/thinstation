f312796cfd99f0dcf28ae0aaabcde21749b8247b
Per-Ole Fanuelsen <per-ole.fanuelsen@horten.kommune.no>
2018-11-29 14:17:40 +0100

    Added buildtime config

--- hkbuild.sh
+++ hkbuild.sh
@@ -1,4 +1,5 @@
 # Build script
 wget https://raw.githubusercontent.com/Hortenkommune/Thinstation/master/build.conf -O gitbuild.conf
+wget https://raw.githubusercontent.com/Hortenkommune/Thinstation/master/thinstation.conf.buildtime -O thinstation.conf.buildtime
 cat gitbuild.conf passwd.conf > build.conf.example
 /bin/bash build
