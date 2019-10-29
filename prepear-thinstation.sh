b072957852378ef10719a10512bbf5a60918b187
fanuelsen@mac.com <fanuelsen@mac.com>
2019-10-29 14:01:10 +0100

    moretesting

--- prepear-thinstation.sh
+++ prepear-thinstation.sh
@@ -5,4 +5,4 @@ touch /tmp/thinstation/ts/etc/READ
 touch /tmp/thinstation/ts/etc/support.love
 cp -a /tmp/hkthin/machine/. /tmp/thinstation/ts/build/machine/
 cp -a /tmp/hkthin/thinstation.conf.buildtime /tmp/thinstation/ts/build/thinstation.conf.buildtime
-cat hkbuild.conf passwd.conf > /tmp/thinstation/ts/build/build.conf.example
+cat /tmp/hkthin/hkbuild.conf /tmp/hkthin/passwd.conf > /tmp/thinstation/ts/build/build.conf.example
