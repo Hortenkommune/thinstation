8d68feaabe6489d2ea7866ac358d2f58b0935ee6
fanuelsen@mac.com <fanuelsen@mac.com>
2019-10-29 13:13:01 +0100

    changes

--- prepear-thinstation.sh
+++ prepear-thinstation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git clone --depth 1 git://github.com/Hortenkommune/thinstation /tmp/hkthin
 git clone --depth 1 git://github.com/Thinstation/thinstation.git -b 6.2-Stable /tmp/thinstation
-touch /tmp/thinstation/ts/etc/READ \
+touch /tmp/thinstation/ts/etc/READ
 touch /tmp/thinstation/ts/etc/support.love
