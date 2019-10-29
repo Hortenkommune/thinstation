d2d92bbc0b966c90656e74ff6afd37c9d8a0ef1d
fanuelsen@mac.com <fanuelsen@mac.com>
2019-10-29 13:55:24 +0100

    added some testing stuff

--- prepear-thinstation.sh
+++ prepear-thinstation.sh
@@ -3,3 +3,6 @@ git clone --depth 1 git://github.com/Hortenkommune/thinstation /tmp/hkthin
 git clone --depth 1 git://github.com/Thinstation/thinstation.git -b 6.2-Stable /tmp/thinstation
 touch /tmp/thinstation/ts/etc/READ
 touch /tmp/thinstation/ts/etc/support.love
+cp -a /tmp/hkthin/machine/. /tmp/thinstation/ts/build/machine/
+cp -a /tmp/hkthin/thinstation.conf.buildtime /tmp/thinstation/ts/build/thinstation.conf.buildtime
+cat hkbuild.conf passwd.conf > /tmp/thinstation/ts/build/build.conf.example
