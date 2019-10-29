f67fd4d8d345e27dec6b76d4e206e2f2f4553ffe
fanuelsen@mac.com <fanuelsen@mac.com>
2019-10-29 20:58:02 +0100

    funnybunny

--- prepear-thinstation.sh
+++ prepear-thinstation.sh
@@ -6,3 +6,5 @@ touch /tmp/thinstation/ts/etc/support.love
 cp -a /tmp/hkthin/machine/. /tmp/thinstation/ts/build/machine/
 cp -a /tmp/hkthin/thinstation.conf.buildtime /tmp/thinstation/ts/build/thinstation.conf.buildtime
 cat /tmp/hkthin/hkbuild.conf /tmp/hkthin/passwd.conf > /tmp/thinstation/ts/build/build.conf.example
+cd /tmp/thinstation
+. setup-chroot -i -b
