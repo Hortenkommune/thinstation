e3a4a77160232dceb9a0fb63cd5f6990a5aff143
fanuelsen@mac.com <fanuelsen@mac.com>
2019-10-29 13:09:16 +0100

    Updatedet script

--- prepear-thinstation.sh
+++ prepear-thinstation.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-echo "hello world!"
+git clone --depth 1 git://github.com/Hortenkommune/thinstation /tmp/hkthin
+git clone --depth 1 git://github.com/Thinstation/thinstation.git -b 6.2-Stable /tmp/thinstation
+touch /tmp/thinstation/ts/etc/READ \
+touch /tmp/thinstation/ts/etc/support.love
