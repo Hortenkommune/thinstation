5e2cbb3348db7acbb848366a085309263d252564
fanuelsen@mac.com <fanuelsen@mac.com>
2019-11-14 21:52:48 +0100

    some changes

--- prepare-thinstation-for-building.sh
+++ prepare-thinstation-for-building.sh
@@ -2,8 +2,6 @@
 basepath=thinstation
 prep=prepare
 bootimages=thinstation/build/boot-images
-buildurl="head -n 2 /data/url.conf"
-buildtimeurl="tail -n 1 /data/url.conf"
 rm -rf /$prep
 git clone --depth 1 git://github.com/Hortenkommune/$basepath /$prep --single-branch --branch master
 cp -TR /$prep/machine/. /$basepath/ts/build/machine/
@@ -12,8 +10,8 @@ cp -TR /$prep/theme/splash/. /$basepath/ts/build/utils/tools/splash/default/
 cp -TR /$prep/theme/wallpaper.jpg /$basepath/ts/build/backgrounds/wallpaper.jpg
 paswd=$(date +%s | sha256sum | base64 | head -c 16 ; echo) 
 echo param rootpasswd $paswd > /data/secret
-cat /$prep/conf/build.conf $buildurl /data/secret > /$basepath/ts/build/build.conf.example
-cat $buildtimeurl > /$basepath/ts/build/thinstation.conf.buildtime
+cat /$prep/conf/build.conf /data/secret > /$basepath/ts/build/build.conf.example
+cat /data/url.conf | head -n2 >> /$basepath/ts/build/build.conf.example && cat /data/url.conf | tail -n1 >> /$basepath/ts/build/thinstation.conf.buildtime
 icabuildurl=$(cat /$basepath/ts/build/build.urls | grep "linuxx")
 icafilename=${icabuildurl#*file://downloads/}
 ahref=$(curl -s https://www.citrix.com/downloads/workspace-app/linux/workspace-app-for-linux-latest.html | grep linuxx64 | grep tar.gz | sed -r 's/^.+rel="([^"]+)".+$/\1/')
