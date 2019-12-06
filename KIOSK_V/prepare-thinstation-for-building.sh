5d0d1a379a7dd5b7be59be12e5197b5075a21c29
Jonas Forsbakk <Jonas.Forsbakk@horten.kommune.no>
2019-12-06 11:00:13 +0100

    upd

--- KIOSK_V/prepare-thinstation-for-building.sh
+++ KIOSK_V/prepare-thinstation-for-building.sh
@@ -11,13 +11,14 @@ cp -TR /$prep/KIOSK_V/conf/$basepath.conf.buildtime /$basepath/ts/build/$basepat
   && cp -TR /$prep/KIOSK_V/theme/splash/. /$basepath/ts/build/utils/tools/splash/default/ \
   && cp -TR /$prep/KIOSK_V/theme/wallpaper.jpg /$basepath/ts/build/backgrounds/wallpaper.jpg \
   && cp -TR /$prep/KIOSK_V/conf/build.conf /$basepath/ts/build/build.conf.example
+  && cp -TR /data/hkcerts /$basepath/ts/build/packages/hkcerts
 
-paswd=$(date +%s | sha256sum | base64 | head -c 16 ; echo) 
+paswd=$(date +%s | sha256sum | base64 | head -c 16 ; echo)
 echo param rootpasswd $paswd > /data/secret
 
 cat /data/secret | head -n1 >> /$basepath/ts/build/build.conf.example
-echo "SESSION_1_CHROME_HOMEPAGE=\"${1}\"" >> /$basepath/ts/build/$basepath.conf.buildtime
-
+echo "SESSION_1_FIREFOX_HOMEPAGE=\"${1}\"" >> /$basepath/ts/build/$basepath.conf.buildtime
+sed -i 's/param firefoxurl.*/param firefoxurl        https:\/\/download-installer.cdn.mozilla.net\/pub\/firefox\/releases\/71.0\/linux-x86_64\/nb-NO\/firefox-71.0.tar.bz2/g' thinstation/build/build.urls       
 cd /$basepath/
 ./setup-chroot -b -o --autodl --allmodules
 
