d9e6a63c462f76706fc31148e449d2bd31f699b0
fanuelsen@mac.com <fanuelsen@mac.com>
2019-12-19 11:46:36 +0100

    Destoyed the system script forbuilding

--- prepare-thinstation-for-building.sh
+++ prepare-thinstation-for-building.sh
@@ -17,8 +17,8 @@ paswd=$(date +%s | sha256sum | base64 | head -c 16 ; echo)
 echo param rootpasswd $paswd > /data/secret
 
 cat /data/url.conf | head -n2 >> /$basepath/ts/build/build.conf.example 
-cat /data/url.conf | tail -n1 >> /$basepath/ts/build/thinstation.conf.buildtime
-cat /data/secret | head -n1 >> /$basepath/ts/build/build.conf.example
+cat /data/url.conf | tail -n2 >> /$basepath/ts/build/thinstation.conf.buildtime
+cat /data/secret >> /$basepath/ts/build/build.conf.example
 
 icabuildurl=$(cat /$basepath/ts/build/build.urls | grep "linuxx")
 icafilename=${icabuildurl#*file://downloads/}
