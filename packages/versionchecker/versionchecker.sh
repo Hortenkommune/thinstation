1e49ce1a07ad765ba14ba6aee0663fec1f264a67
fanuelsen@mac.com <fanuelsen@mac.com>
2020-01-02 16:01:59 +0100

    changes to versionchecker

--- packages/versionchecker/versionchecker.sh
+++ packages/versionchecker/versionchecker.sh
@@ -3,7 +3,9 @@ serverip=$(export | grep SERVER_IP | cut -d '"' -f2)
 currentver="$(grep HDUPDATE_WS /etc/thinstation.defaults | sed 's/^HDUPDATE_WS_VERSION=//')"
 requiredver="$(curl -s ${serverip}/thinstation.conf.network | grep HDUPDATE_SERVER | sed 's/^HDUPDATE_SERVER_VERSION=//')"
  if [ "$(printf '%s\n' "$requiredver" "$currentver" | sort -V | head -n1)" = "$requiredver" ]; then 
-        echo "Greater than or equal to serverversion"
+        echo "Current server version is $requiredver, installed version is $currentver"
+        echo "No updated needed, continuing"
  else
-        echo "Less than Server"
+        echo "Current server version is $requiredver, installed version is $currentver"
+        echo "Update is needed, executing update crew"
  fi
