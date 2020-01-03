c7cda97199c88680b6b94231d3862505c95b531a
fanuelsen@mac.com <fanuelsen@mac.com>
2020-01-03 14:41:07 +0100

    Added fullpath to binaries

--- packages/versionchecker/bin/versionchecker.sh
+++ packages/versionchecker/bin/versionchecker.sh
@@ -3,9 +3,9 @@ set -e
 set -o pipefail
 service="wfica"
 servicename="Citrix Workspace"
-serverip=$(export | grep SERVER_IP | cut -d '"' -f2)
-currentver="$(grep HDUPDATE_WS /etc/thinstation.defaults | sed 's/^HDUPDATE_WS_VERSION=//')"
-requiredver="$(curl -s ${serverip}/thinstation.conf.network | grep HDUPDATE_SERVER | sed 's/^HDUPDATE_SERVER_VERSION=//')"
+serverip=$(export | /bin/grep SERVER_IP | /bin/busybox/cut -d '"' -f2)
+currentver="$(/bin/grep HDUPDATE_WS /etc/thinstation.defaults | /bin/busybox/sed 's/^HDUPDATE_WS_VERSION=//')"
+requiredver="$(/bin/curl -s ${serverip}/thinstation.conf.network | /bin/grep HDUPDATE_SERVER | /bin/busybox/sed 's/^HDUPDATE_SERVER_VERSION=//')"
 
 if [ -z "$requiredver" ]
 then
@@ -15,14 +15,14 @@ else
       echo "Required version variable is NOT empty, continuing..."
 fi
 
-if [ "$(printf '%s\n' "$requiredver" "$currentver" | sort -V | head -n1)" = "$requiredver" ]; then 
+if [ "$(/bin/busybox/printf '%s\n' "$requiredver" "$currentver" | /fastboot/bin/sort -V | /bin/busybox/head -n1)" = "$requiredver" ]; then 
         echo "Current server version is $requiredver, installed version is $currentver"
         echo "No updated needed, continuing..."
 else
         echo "Current server version is $requiredver, installed version is $currentver"
         echo "Update is needed, executing update crew..."
         echo "Checking if $servicename is still running"
-        while pgrep "$service" > /dev/null ; do sleep 10 ; echo "$servicename is still active." ; done
+        while /bin/busybox/pgrep "$service" > /dev/null ; do /bin/busybox/sleep 10 ; echo "$servicename is still active." ; done
         echo "This system needs to reboot to update."
         /sbin/reboot
 fi
