e64816acb0dff95a5aa5391a10b0625d4c23356d
fanuelsen@mac.com <fanuelsen@mac.com>
2020-01-03 08:08:58 +0100

    added reboot logic to test

--- packages/versionchecker/versionchecker.sh
+++ packages/versionchecker/versionchecker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+filetocheck="/tmp/ica_receiver_clear_crentials_alive"
 serverip=$(export | grep SERVER_IP | cut -d '"' -f2)
 currentver="$(grep HDUPDATE_WS /etc/thinstation.defaults | sed 's/^HDUPDATE_WS_VERSION=//')"
 requiredver="$(curl -s ${serverip}/thinstation.conf.network | grep HDUPDATE_SERVER | sed 's/^HDUPDATE_SERVER_VERSION=//')"
@@ -8,4 +9,6 @@ requiredver="$(curl -s ${serverip}/thinstation.conf.network | grep HDUPDATE_SERV
  else
         echo "Current server version is $requiredver, installed version is $currentver"
         echo "Update is needed, executing update crew"
+        while [ -f $filetocheck ] ; do sleep 15; done
+        /sbin/reboot
  fi
