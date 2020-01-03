0ead05e87e4e10a28076b81c987bd899b9e1b6bc
fanuelsen@mac.com <fanuelsen@mac.com>
2020-01-03 08:45:56 +0100

    Added some more code

--- packages/versionchecker/versionchecker.sh
+++ packages/versionchecker/versionchecker.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-filetocheck="/tmp/ica_receiver_clear_credentials_alive"
 serverip=$(export | grep SERVER_IP | cut -d '"' -f2)
 currentver="$(grep HDUPDATE_WS /etc/thinstation.defaults | sed 's/^HDUPDATE_WS_VERSION=//')"
 requiredver="$(curl -s ${serverip}/thinstation.conf.network | grep HDUPDATE_SERVER | sed 's/^HDUPDATE_SERVER_VERSION=//')"
- if [ "$(printf '%s\n' "$requiredver" "$currentver" | sort -V | head -n1)" = "$requiredver" ]; then 
+
+if [ "$(printf '%s\n' "$requiredver" "$currentver" | sort -V | head -n1)" = "$requiredver" ]; then 
         echo "Current server version is $requiredver, installed version is $currentver"
-        echo "No updated needed, continuing"
- else
+        echo "No updated needed, continuing."
+else
         echo "Current server version is $requiredver, installed version is $currentver"
         echo "Update is needed, executing update crew"
-        while [ -f $filetocheck ] ; do sleep 15; done
-        /sbin/reboot
- fi
+        while [ -f /tmp/ica_receiver_clear_credentials_alive ] ; echo "Citrix Workspace is still active." ; do sleep 15; done
+        echo "This system needs to reboot to update."
+        #/sbin/reboot
+fi
