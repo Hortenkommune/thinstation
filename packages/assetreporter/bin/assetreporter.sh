db192d2d379caf6e61b0ec415ecbd81980a6dca7
Jonas Forsbakk <Jonas.Forsbakk@horten.kommune.no>
2020-01-20 15:44:32 +0100

    added package to use extended api in tc

---
+++ packages/assetreporter/bin/assetreporter.sh
@@ -0,0 +1,6 @@
+﻿#!/bin/bash
+host="$(grep "HDUPDATE_SERVER=" /etc/thinstation.defaults | sed 's/^HDUPDATE_SERVER=//')"
+ip="$(ifconfig | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | head -1)"
+now="$(date +%d/%m/%Y-%T)"
+
+curl -X PUT -H "Content-Type: application/json" -d "{\"lastseen\": \"${now}\",\"lastknownip\":\"${ip}\"}" "http://${host}/thinclient/$(hostname)"
