9d9a8fa7264fe759fe1fb2a2cef2165c49be0261
Jonas Forsbakk <Jonas.Forsbakk@horten.kommune.no>
2020-01-21 13:17:45 +0100

    added wsversion

--- packages/assetreporter/bin/assetreporter.sh
+++ packages/assetreporter/bin/assetreporter.sh
@@ -2,5 +2,9 @@
 host="$(grep "HDUPDATE_SERVER=" /etc/thinstation.defaults | sed 's/^HDUPDATE_SERVER=//')"
 ip="$(ifconfig | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | head -1)"
 now="$(date +%d/%m/%Y-%T)"
+wsver="$(grep "HDUPDATE_WS" /etc/thinstation.defaults | sed 's/^HDUPDATE_WS_VERSION=//')"
 
-curl -X PUT -H "Content-Type: application/json" -d "{\"lastseen\": \"${now}\",\"lastknownip\":\"${ip}\"}" "http://${host}/thinclient/$(hostname)"
+jsondata='{"lastseen": "%s","lastknownip":"%s","wsversion": "%s"}\n'
+body="$(printf "$jsondata" "$now" "$ip" "$wsver")"
+
+curl -X PUT -H "Content-Type: application/json" -d "${body}" "http://${host}/thinclient/$(hostname)"
