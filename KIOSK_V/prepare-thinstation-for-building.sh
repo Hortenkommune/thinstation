42e24a09775fd53599de7e4ab4f12a6211827897
Jonas Forsbakk <Jonas.Forsbakk@horten.kommune.no>
2020-03-05 09:56:28 +0100

    more test

--- KIOSK_V/prepare-thinstation-for-building.sh
+++ KIOSK_V/prepare-thinstation-for-building.sh
@@ -7,15 +7,12 @@ rm -rf /$prep
 
 git clone --depth 1 git://github.com/Hortenkommune/$basepath /$prep --single-branch --branch master
 
-#Removed
-#&& cp -TR /data/iwlwifi /$basepath/ts/build/packages/iwlwifi \
-#sed -i 's/param firefoxurl.*/param firefoxurl        https:\/\/download.mozilla.org\/?product=firefox-latest\&os=linux64\&lang=nb-NO/g' /thinstation/build/build.urls
-
 cp -TR /$prep/KIOSK_V/conf/$basepath.conf.buildtime /$basepath/ts/build/$basepath.conf.buildtime \
   && cp -TR /$prep/KIOSK_V/theme/splash/. /$basepath/ts/build/utils/tools/splash/default/ \
   && cp -TR /$prep/KIOSK_V/theme/wallpaper.jpg /$basepath/ts/build/backgrounds/wallpaper.jpg \
   && cp -TR /$prep/KIOSK_V/conf/build.conf /$basepath/ts/build/build.conf.example \
   && cp -TR /$prep/KIOSK_V/packages/firefox/build/install /$basepath/ts/build/packages/firefox/build/install \
+  && cp -TR /data/iwlwifi /$basepath/ts/build/packages/iwlwifi \
   && cp -TR /data/hkcerts /$basepath/ts/build/packages/hkcerts \
   && cp -TR /data/firefox-policies.json /$basepath/ts/build/packages/firefox/policies.json
 
@@ -24,6 +21,7 @@ echo param rootpasswd $paswd > /data/secret
 
 cat /data/secret | head -n1 >> /$basepath/ts/build/build.conf.example
 echo "SESSION_1_FIREFOX_HOMEPAGE=\"${1}\"" >> /$basepath/ts/build/$basepath.conf.buildtime
+sed -i 's/param firefoxurl.*/param firefoxurl        https:\/\/download.mozilla.org\/?product=firefox-latest\&os=linux64\&lang=nb-NO/g' /thinstation/build/build.urls
 cat /data/wlan >> /$basepath/ts/build/$basepath.conf.buildtime
 cd /$basepath/
 ./setup-chroot -b -o --autodl --allmodules
