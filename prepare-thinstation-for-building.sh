26bee56a53b048aa17cc733887cf2d615cea19c2
Per-Ole <fanuelsen@mac.com>
2021-02-01 19:56:36 +0100

    Update prepare-thinstation-for-building.sh
    adding allmodules build

--- prepare-thinstation-for-building.sh
+++ prepare-thinstation-for-building.sh
@@ -38,7 +38,7 @@ chmod +x /$basepath/ts/build/packages/versionchecker/bin/versionchecker.sh
 chmod +x /$basepath/ts/build/packages/assetreporter/bin/assetreporter.sh
 
 cd /$basepath/
-./setup-chroot -b -o --autodl
+./setup-chroot -b -o --autodl --allmodules
 
 if [ ! -d "/data/boot-images" ]; then
   mkdir /data/boot-images
