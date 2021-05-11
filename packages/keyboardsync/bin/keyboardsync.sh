709ab399c33d1f1b9326af1ade88eb93a3d92f11
fanuelsen <fanuelsen@mac.com>
2021-05-11 08:42:54 +0200

    trying to slove with script instead

--- packages/keyboardsync/bin/keyboardsync.sh
+++ packages/keyboardsync/bin/keyboardsync.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-/lib/bin/setxkbmap -synch -display :0 -verbose
+processname="xfce4-session"
+process="$(ps aux | grep -v "grep" | grep $processname)"
+if [ -z "$process" ]
+then
+	while pgrep "$processname" > /dev/null ; do sleep 2 ; echo "$processname is not active." ; done
+else
+    /bin/setxkbmap -synch -display :0 -verbose
+fi
