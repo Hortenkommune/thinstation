fd90335427111fbd3a3ac0be7c5a6855bf69df73
Per-Ole Fanuelsen <prolle@intern.i-sone.no>
2022-05-16 16:18:17 +0200

    trying to build kernel

--- prepare-thinstation-for-building.sh
+++ prepare-thinstation-for-building.sh
@@ -30,7 +30,7 @@ tsuserpasswd=$(date +%s | sha256sum | base64 | head -c 16 ; echo)
 echo param rootpasswd $rootpasswd >> /data/secret
 echo param tsuserpasswd $tsuserpasswd >> /data/secret
 # Kernelstuff
-echo 5.15.40 >> /$basepath/ts/ports/kernel-modules/VERSION
+echo 5.15.40 > /$basepath/ts/ports/kernel-modules/VERSION
 #####
 cat /data/url.conf | head -n2 >> /$basepath/ts/build/build.conf.example 
 cat /data/url.conf | tail -n3 >> /$basepath/ts/build/thinstation.conf.buildtime
@@ -50,7 +50,8 @@ chmod +x /$basepath/ts/build/packages/versionchecker/bin/versionchecker.sh
 chmod +x /$basepath/ts/build/packages/assetreporter/bin/assetreporter.sh
 
 cd /$basepath/
-./setup-chroot -b -a -e rebuild-kernels -a
+./setup-chroot -e "rebuild-kernels -a"
+./setup-chroot -b -o --autodl
 
 if [ ! -d "/data/boot-images" ]; then
   mkdir /data/boot-images
