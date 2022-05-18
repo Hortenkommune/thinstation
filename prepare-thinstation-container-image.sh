e6bfc3e102d39f1202de728ba733d6b20615197b
Per-Ole Fanuelsen <prolle@intern.i-sone.no>
2022-05-18 13:12:00 +0200

    added kernel to image

--- prepare-thinstation-container-image.sh
+++ prepare-thinstation-container-image.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+basepath=thinstation
+kernelversion=5.15.40
 git clone --depth 1 https://github.com/Thinstation/thinstation.git /thinstation --single-branch --branch 6.2-Stable
 touch /thinstation/ts/etc/READ
 touch /thinstation/ts/etc/support.love
-cd /thinstation/
+cd /$basepath
+echo $kernelversion > /$basepath/ts/ports/kernel-modules/VERSION
 ./setup-chroot -i
+./setup-chroot -e "rebuild-kernels -a"
