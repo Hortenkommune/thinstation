8d0b166e279ac00472ef8c9fea3a0e3d348e29da
Per-Ole Fanuelsen <prolle@intern.i-sone.no>
2022-05-27 08:35:17 +0200

    trying to move kernel building into image

--- prepare-thinstation-container-image.sh
+++ prepare-thinstation-container-image.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 basepath=thinstation
-git clone --depth 1 https://github.com/Thinstation/thinstation.git /thinstation --single-branch --branch 6.2-Stable
-touch /thinstation/ts/etc/READ
-touch /thinstation/ts/etc/support.love
-cd /$basepath
+kernelversion=5.15.41
 
+git clone --depth 1 https://github.com/Thinstation/thinstation.git /$basepath --single-branch --branch 6.2-Stable
+touch /$basepath/ts/etc/READ
+touch /$basepath/ts/etc/support.love
+echo $kernelversion > /$basepath/ts/ports/kernel-modules/VERSION
+
+cd /$basepath
 ./setup-chroot -i
+./setup-chroot -e "rebuild-kernels -a"
