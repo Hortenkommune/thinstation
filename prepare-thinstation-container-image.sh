bd73731fa1aabc092f0ee98970782d4147f72eda
Per-Ole <fanuelsen@mac.com>
2022-06-02 14:19:33 +0200

    Update prepare-thinstation-container-image.sh

--- prepare-thinstation-container-image.sh
+++ prepare-thinstation-container-image.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 wget https://github.com/Thinstation/thinstation/archive/refs/tags/6.2.13.zip
-unzip -q 6.2.13.zip
+mkdir thinstation
+cd thinstation
+bsdtar --strip-components=1 -xvf /*.zip
 #git clone --depth 1 https://github.com/Thinstation/thinstation.git /thinstation --single-branch --branch 6.2-Stable
 touch /thinstation/ts/etc/READ
 touch /thinstation/ts/etc/support.love
